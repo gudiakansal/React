4e2e85f03cc6f1de9fd2aa4003531ef079c3bc86
Gudia Kansal <gudiakansal248@gmail.com>
2025-12-08 14:25:06 +0530

    initial commit

--- Day01/app.js
+++ Day01/app.js
@@ -54,9 +54,55 @@
 //     }
 // }
 
+// ORIGINAL REACT 
+const React={
+    createElement(type,props,children){
+return {
+    type:type,
+    props:{
+        ...props,
+        children:children
+    }
+}
+}
+}
+// const reactElement={
+//     type:'h1',
+//     props:{
+//         className:"element",
+//         id:"first",
+//         children:"hello"
+//     }
+// }
+const ReactDOM={
+    render:function(reactElement,root){
+        root.innerHTML='';
+       const element= document.createElement(reactElement.type);
+       const{props}=reactElement;
+       for(const key in props){
+        if(key==='style'){
+            Object.assign(element.style,props.style);
+        }
+        else if (key==='children'){
+            element.textContent=props[key];
+        }
+        else{
+            element[key]=props[key];
+        }
+       }
+       root.append(element);
+    }
+}
 const element1=React.createElement("h1",{className:"element",id:"first",style:{fontSize:"30px",backgroundColor:"orange",color:"white"}},"hello");
 const element2=React.createElement("h2",{className:"element",id:"second",style:{fontSize:"30px",backgroundColor:"orange",color:"white"}},"gudia");
 
-const root=document.getElementById('root');
-ReactDOM.render(element1,root);
-ReactDOM.render(element2,root);
+const div=React.createElement('div',null,element1,element2,"hello");
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(div);
+//console.log(element1);
+// ReactDOM.render(element1,document.getElementById('root'));
+// ReactDOM.render(element2,document.getElementById('root'));
+//const root=document.getElementById('root');
+//ReactDOM.render(element1,root);
+//ReactDOM.render(element2,root);
