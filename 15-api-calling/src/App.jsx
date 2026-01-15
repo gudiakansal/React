58e77a6f46838b1623bec1322f15251664ed7150
Gudia Kansal <gudiakansal248@gmail.com>
2026-01-15 16:16:41 +0530

    initial commit

--- 15-api-calling/src/App.jsx
+++ 15-api-calling/src/App.jsx
@@ -1,34 +1,27 @@
+import axios from 'axios'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const [data, setData] = useState([])
+
+  const getData = async () => {
+    
+    const response = await axios.get('https://picsum.photos/v2/list')
+
+    setData(response.data)
+    
+  }
   return (
-    <>
+    <div>
+      <button onClick={getData}>Get Data</button>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {data.map(function(elem,idx){
+
+          return <h3>Hello, {elem.author} {idx}</h3>
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
+
+  function aChanging() {
+    console.log('A ki value change ho gyi');
+  }
+
+  function bChanging() {
+    console.log('B ki value change ho gyi');
+  }
+
+  useEffect(function(){
+    aChanging()
+  },[a])
+  
+  useEffect(function(){
+    bChanging()
+  },[b])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+      <button
+        onClick={() => {
+          setA(a + 1)
+        }}
+      >Change A</button>
+      <button
+        onClick={() => {
+          setB(b - 1)
+        }}
+      >Change B</button>
+    </div>
   )
 }
 
